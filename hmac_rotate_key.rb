926b6f2c39cc362da93be4f192f0eeac4907f133
Dan Moore <github@mooreds.com>
2020-05-19 15:51:44 +0000

    Added key rotation example.

---
+++ hmac_rotate_key.rb
@@ -0,0 +1,24 @@
+require 'jwt'
+
+hmac_secret = 'helloKCRuby!##'
+
+exp = Time.now.to_i + (5*60)
+
+payload = { "iss": "fusionauth.io", 
+            "exp": exp, 
+            "aud": "238d4793-70de-4183-9707-48ed8ecd19d9",
+            "sub": "19016b73-3ffa-4b26-80d8-aa9287738677",
+            "name": "Dan Moore",
+            "roles": ["RETRIEVE_TODOS"]
+}
+
+token = JWT.encode payload, hmac_secret, 'HS256'
+
+puts token
+
+decoded_token = JWT.decode token, hmac_secret, true, { algorithm: 'HS256' }
+
+puts decoded_token
+
+new_hmac_secret = 'byeKCRuby!##'
+decoded_token = JWT.decode token, new_hmac_secret, true, { algorithm: 'HS256' }
